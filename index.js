1deeed84b0bcfd2e700989d07ae63a9bfeebecf1
Austin Baik <baik.austin@gmail.com>
2021-12-20 20:26:42 -0800

    commit tree 21fc8731edb3d0c1f58f4507382bfce4f5e639cb

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(someValue) { 
+    return Math.abs(someValue - 42);
+    //distance from 42nd street by arithmetic and absolute value
+}
+
+
+function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+    // call the distanceFromHqInBlocks function from inside the distanceFromHqInFeet function,
+    // passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
+  
+    // the return value of distanceFromHqInBlocks can then be used to calculate feet
+  }
+  
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+  }
+
+
+function calculatesFarePrice(start, destination) {
+    //returns the fare for the customer
+    
+    distanceTravelledInFeet(start, destination)
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) -400) * .02;
+    }
+    // charges 2 cents per foot when total feet travelled is between 400 and 2000 (remember the first 400 feet are free!):
+    else if (distanceTravelledInFeet(start, destination) <= 2500) {
+        return 25;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far';
+    }
+  }
